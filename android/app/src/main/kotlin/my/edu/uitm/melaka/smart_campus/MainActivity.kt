90f15501fe1c449ed8675dc7001ad6c326a1cb07
ibnuyahya <ibnuyahya@gmail.com>
2020-02-18 07:12:45 +0800

    first commit

---
+++ android/app/src/main/kotlin/my/edu/uitm/melaka/smart_campus/MainActivity.kt
@@ -0,0 +1,12 @@
+package my.edu.uitm.melaka.smart_campus
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
